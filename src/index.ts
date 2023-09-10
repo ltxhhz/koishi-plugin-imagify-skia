1f01792e25df8cc6921c7c7683c08abd3e11cdf0
ltxhhz <ltxhhz@qq.com>
2023-09-10 18:58:22 +0800

    9.10 使用 skia 服务

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,25 @@
-import { Context, Schema, h, version } from 'koishi'
-import { Canvas } from 'skia-canvas'
+import { Context, Schema, h } from 'koishi'
+import type { Canvas } from '@ltxhhz/koishi-plugin-skia-canvas'
+
 export const name = 'imagify-skia'
 
-type Ground = {
-  type: 'color',
-  color?: string
-} | {
-  type: 'linearGradient',
-  angle: number,
-  colorStop: [number, string][]
-} | {
-  type: 'pattern',
-  pattern: any
-} | {
-  type?: ''
-}
+type Ground =
+  | {
+      type: 'color'
+      color?: string
+    }
+  | {
+      type: 'linearGradient'
+      angle: number
+      colorStop: [number, string][]
+    }
+  | {
+      type: 'pattern'
+      pattern: any
+    }
+  | {
+      type?: ''
+    }
 
 // type Ground = {
 //   type: 'color' | 'linearGradient' | 'pattern'
@@ -29,16 +34,16 @@ export interface Config {
   maxLength: number
   // background: string
   // blur: number,
-  imageMinWidth: number,
+  imageMinWidth: number
   imageMaxWidth: {
-    auto?: false,
-    maxWidth?: number,
-  },
-  lineHeight: number,
-  fontSize: number,
-  font: string,
-  padding: Record<'left' | 'right' | 'top' | 'bottom', number>,
-  background: Ground,
+    auto?: false
+    maxWidth?: number
+  }
+  lineHeight: number
+  fontSize: number
+  font: string
+  padding: Record<'left' | 'right' | 'top' | 'bottom', number>
+  background: Ground
   foreground: Ground
 }
 
@@ -56,7 +61,7 @@ export const Config: Schema<Config> = Schema.object({
         maxWidth: Schema.number().min(300).default(400).description('图片最大宽度，单位 px')
       }),
       Schema.object({})
-    ]),
+    ])
   ]),
   lineHeight: Schema.number().min(0).default(0).description('文字行高，为0则不设置'),
   fontSize: Schema.number().min(0).default(20).description('文字大小'),
@@ -69,7 +74,7 @@ export const Config: Schema<Config> = Schema.object({
   }),
   background: Schema.intersect([
     Schema.object({
-      type: Schema.union<'color' | 'linearGradient' | 'pattern'>(['color', 'linearGradient'/* , 'pattern' */]).description('是时候学习了： [fillStyle](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/fillStyle)'),
+      type: Schema.union<'color' | 'linearGradient' | 'pattern'>(['color', 'linearGradient' /* , 'pattern' */]).description('是时候学习了： [fillStyle](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/fillStyle)')
     }),
     Schema.union([
       Schema.object({
@@ -79,10 +84,7 @@ export const Config: Schema<Config> = Schema.object({
       Schema.object({
         type: Schema.const('linearGradient').required(),
         angle: Schema.number().min(0).max(359).default(0).description('角度，单位 deg'),
-        colorStop: Schema.array(Schema.tuple([
-          Schema.number().min(0).max(1).step(0.01).default(0).role('slider').description('offset'),
-          Schema.string().description('color')
-        ]).description('[偏移量,颜色]')).description('是时候学习了 [CanvasGradient.addColorStop](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasGradient/addColorStop)')
+        colorStop: Schema.array(Schema.tuple([Schema.number().min(0).max(1).step(0.01).default(0).role('slider').description('offset'), Schema.string().description('color')]).description('[偏移量,颜色]')).description('是时候学习了 [CanvasGradient.addColorStop](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasGradient/addColorStop)')
       }),
       Schema.object({
         type: Schema.const('pattern').required(),
@@ -93,7 +95,7 @@ export const Config: Schema<Config> = Schema.object({
   ]).description('背景填充'),
   foreground: Schema.intersect([
     Schema.object({
-      type: Schema.union<'color' | 'linearGradient' | 'pattern'>(['color', 'linearGradient'/* , 'pattern' */]).description('是时候学习了： [fillStyle](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/fillStyle)'),
+      type: Schema.union<'color' | 'linearGradient' | 'pattern'>(['color', 'linearGradient' /* , 'pattern' */]).description('是时候学习了： [fillStyle](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasRenderingContext2D/fillStyle)')
     }),
     Schema.union([
       Schema.object({
@@ -103,10 +105,7 @@ export const Config: Schema<Config> = Schema.object({
       Schema.object({
         type: Schema.const('linearGradient').required(),
         angle: Schema.number().min(0).max(359).default(0).description('角度，单位 deg'),
-        colorStop: Schema.array(Schema.tuple([
-          Schema.number().min(0).max(1).step(0.01).default(0).role('slider').description('offset'),
-          Schema.string().description('color')
-        ]).description('[偏移量,颜色]')).description('是时候学习了 [CanvasGradient.addColorStop](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasGradient/addColorStop)')
+        colorStop: Schema.array(Schema.tuple([Schema.number().min(0).max(1).step(0.01).default(0).role('slider').description('offset'), Schema.string().description('color')]).description('[偏移量,颜色]')).description('是时候学习了 [CanvasGradient.addColorStop](https://developer.mozilla.org/zh-CN/docs/Web/API/CanvasGradient/addColorStop)')
       }),
       Schema.object({
         type: Schema.const('pattern').required(),
@@ -114,13 +113,14 @@ export const Config: Schema<Config> = Schema.object({
       }),
       Schema.object({})
     ])
-  ]).description('前景填充'),
+  ]).description('前景填充')
 })
 
-export const using = []
+export const using = ['skia']
 
 export function apply(ctx: Context, config: Config) {
-  ctx.before('send', (session) => {
+  const { Canvas } = ctx.skia
+  ctx.before('send', session => {
     const content = h.unescape(session.content)
     let contentMapper = [content]
     if (content.includes('\n')) {
@@ -130,7 +130,7 @@ export function apply(ctx: Context, config: Config) {
       const can = new Canvas()
       const ctx = can.getContext('2d')
       const run = () => {
-        ctx.textAlign = "left"
+        ctx.textAlign = 'left'
         ctx.textBaseline = 'top'
         ctx.textWrap = true
         ctx.font = `${config.fontSize}px${config.lineHeight ? `/${config.lineHeight}px` : ''} ${config.font}`
@@ -176,18 +176,18 @@ export function apply(ctx: Context, config: Config) {
   })
 }
 
-
 function calcGradual(angle: number, canvas: Canvas): [number, number, number, number] {
-  angle = angle * Math.PI / 180
+  angle = (angle * Math.PI) / 180
   const centerX = canvas.width / 2
   const centerY = canvas.height / 2
 
   const length = Math.sqrt(canvas.width ** 2 + canvas.height ** 2)
 
-  const x0 = centerX - length / 2 * Math.cos(angle)
-  const y0 = centerY - length / 2 * Math.sin(angle)
+  const x0 = centerX - (length / 2) * Math.cos(angle)
+  const y0 = centerY - (length / 2) * Math.sin(angle)
 
-  const x1 = centerX + length / 2 * Math.cos(angle)
-  const y1 = centerY + length / 2 * Math.sin(angle)
+  const x1 = centerX + (length / 2) * Math.cos(angle)
+  const y1 = centerY + (length / 2) * Math.sin(angle)
   return [x0, y0, x1, y1]
 }
+
